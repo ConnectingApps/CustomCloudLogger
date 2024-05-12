4649305c155cfc833e435441d0d8bc5d65d6a778
Daan Acohen <daan.acohen@connectingapps.net>
2024-05-12 18:25:28 +0200

    Test the availability of environment variables

--- ConnectingApps.CustomCloudLogger.Test/UnitTest1.cs
+++ ConnectingApps.CustomCloudLogger.Test/UnitTest1.cs
@@ -1,9 +1,30 @@
+using FluentAssertions;
+
 namespace ConnectingApps.CustomCloudLogger.Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    private readonly string _workSpaceId;
+    private readonly string _sharedKey;
+
+    public UnitTest1()
     {
+        _workSpaceId = Environment.GetEnvironmentVariable("WORKSPACE_ID")!;
+        _sharedKey = Environment.GetEnvironmentVariable("SHARED_KEY")!;
+    }
+    
+    [Fact]
+    public void WorkSpaceIdTest()
+    {
+        _workSpaceId.Should().NotBeNullOrEmpty("WORKSPACE_ID environment variable must be set");
+        _workSpaceId.Length.Should().Be(36);
+    }
+    
+    
+    [Fact]
+    public void SharedKeyTest()
+    {
+        _sharedKey.Should().NotBeNullOrEmpty("SHARED_KEY environment variable must be set");
+        _sharedKey.Length.Should().Be(88);
     }
 }
